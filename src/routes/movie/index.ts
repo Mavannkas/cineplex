56874b5b464336caaebb05854606432e7f5dfcd0
Wiktor Ludwiniak <wludwiniak@tbscg.com>
2023-12-10 15:46:01 +0100

    Example endpoint

---
+++ src/routes/movie/index.ts
@@ -0,0 +1,21 @@
+import { FastifyPluginAsync } from "fastify";
+
+interface ParamsWithID {
+    id: string;
+}
+
+const isParamsWithID = (params: unknown): params is ParamsWithID => {
+    return typeof params === "object" && params !== null && "id" in params;
+}
+
+const plugin: FastifyPluginAsync = async (fastify, opts) => {
+    fastify.get("/:id", async (request, reply) => {
+        if (!isParamsWithID(request.params)) {
+            throw fastify.httpErrors.badRequest()
+        }
+
+        return request.params.id;
+    })
+}
+
+export default plugin;
