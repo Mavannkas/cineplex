446c257e1de5f762c3923d7e3db60e342ffd76d7
Cezary Radecki <radecki.cezary.it@gmail.com>
2024-01-10 21:45:10 +0100

    Update crud for movie adding better searching

--- src/model/movie/movie.manager.ts
+++ src/model/movie/movie.manager.ts
@@ -1,28 +1,41 @@
 import { MySQLPromisePool } from "@fastify/mysql";
 import { getSingleItem } from "../../utils/manager.utils.js";
 import { Movie } from "./movie.model.js";
-import { PaginationParams } from "../../utils/search.utils.js";
+import {
+  PaginationParams,
+  searchMoviePaginationParamsConditions,
+} from "../../utils/search.utils.js";
 
 export default class MovieManager {
   constructor(private mysql: MySQLPromisePool) {}
 
-  async getMovieById(id: string): Promise<Movie> {
-    const [item] = await this.mysql.query("SELECT * FROM Movies WHERE id = ?", [
-      id,
-    ]);
+  async getMovie(params?: MoviePaginationParams) {
+    const { query, queryParams } =
+      searchMoviePaginationParamsConditions(params);
 
+    const sqlQuery = `SELECT * FROM Movies ${query}`;
+
+    const [item] = await this.mysql.query(sqlQuery, queryParams);
     return getSingleItem(item);
   }
 
-  async getMovie(params?: PaginationParams) {
-    const [item] = await this.mysql.query();
-  }
-
   async createMovie(data: Omit<Movie, "id">) {
-    await this.mysql.query("INSERT INTO Movie SET ?", [data]);
+    await this.mysql.query("INSERT INTO Movies SET ?", [data]);
   }
 
   async deleteMovie(id: string) {
-    await this.mysql.query("DELETE FROM Movie WHERE id = ?", [id]);
+    await this.mysql.query("DELETE FROM Movies WHERE id = ?", [id]);
   }
+}
+
+interface MoviePaginationParams extends PaginationParams {
+  title?: string;
+  description?: string;
+  minimum_age?: number;
+  production_year?: number;
+  duration?: number;
+  director?: string;
+  production_country?: string;
+  premiere_date?: Date;
+  cast?: string;
 }
--- src/utils/search.utils.ts
+++ src/utils/search.utils.ts
@@ -1,14 +1,42 @@
+import { MoviePaginationParams } from "../model/movie/movie.manager.js";
+
 export interface PaginationParams {
-    page?: number;
-    limit?: number;
+  page?: number;
+  limit?: number;
 }
 
 export const isPaginationParams = (obj: any): obj is PaginationParams => {
-    return typeof obj === 'object' && (obj.page || obj.limit);
-}
+  return typeof obj === "object" && (obj.page || obj.limit);
+};
 
-export const queryWithPagination = (query: string, params: (string | number)[], pagination?: PaginationParams): [string, (string | number)[]] => {
-    const { page = 0, limit = 10 } = pagination ?? {};
-    const offset = (page) * limit;
-    return [`${query} LIMIT ?, ?`, [...params, +offset, +limit]];
+export const queryWithPagination = (
+  query: string,
+  params: (string | number)[],
+  pagination?: PaginationParams
+): [string, (string | number)[]] => {
+  const { page = 0, limit = 10 } = pagination ?? {};
+  const offset = page * limit;
+  return [`${query} LIMIT ?, ?`, [...params, +offset, +limit]];
+};
+
+export function searchMoviePaginationParamsConditions(
+  params?: MoviePaginationParams
+): {
+  query: string;
+  queryParams: any[];
+} {
+  const conditions: string[] = [];
+  const queryParams: any[] = [];
+
+  Object.keys(params).forEach((param) => {
+    if (params[param]) {
+      conditions.push(`${param} LIKE ?`);
+      queryParams.push(`%${params[param]}%`);
+    }
+  });
+
+  const query =
+    conditions.length > 0 ? 'WHERE ${conditions.join(" AND ")}' : "";
+
+  return { query, queryParams };
 }
